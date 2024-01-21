ca3cb169896af2a3157e1bc0a7ed628ea92bd358
DonggeJia <doj14@pitt.edu>
2024-01-20 19:09:03 -0500

    1

--- docs/make.jl
+++ docs/make.jl
@@ -17,7 +17,13 @@ makedocs(;
     ],
 )
 
-deploydocs(;
-    repo="github.com/DonggeJia/try1",
-    devbranch="main"
+# deploydocs(;
+#     repo="github.com/DonggeJia/try1",
+#     devbranch="main"
+# )
+deploydocs(
+    repo="github.com/DonggeJia/try1.git",
+    devbranch="main",
+    tag = isdefined(Main, :TAG) ? Main.TAG : "",
+    push_preview = true, # Optional, for previewing changes in pull requests
 )
